9bfb995b7c50ff0326df1a42ef53080650395273
huzaifa786789 <huzaifa786789@gmail.com>
2024-07-10 07:53:05 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,18 @@
 provider "aws" {
   region = "us-east-1"
-  # Add other provider configurations as needed
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  tags = {
-    Name = "example-instance"
-  }
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
 }
