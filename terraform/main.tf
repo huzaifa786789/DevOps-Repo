32709858038de9ce0d15093e299fbd5078c3724d
huzaifa786789 <huzaifa786789@gmail.com>
2024-07-10 11:06:01 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,15 +15,34 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+}
+
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
 }
 
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
 resource "aws_instance" "my_ec2_instance" {
-  ami           = "ami-0cff7528ff583bf9a"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.my_subnet.id
+  ami                         = "ami-0cff7528ff583bf9a"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.my_subnet.id
+  associate_public_ip_address = true
 
   provisioner "local-exec" {
     command = <<EOT
