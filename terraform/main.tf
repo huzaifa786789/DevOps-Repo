e47c56e6ec1c1b95640f3578203f505e405cae53
huzaifa786789 <huzaifa786789@gmail.com>
2024-07-10 07:39:29 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,13 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc"
-  }
+provider "aws" {
+  region = "us-east-1"
+  # Add other provider configurations as needed
 }
 
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"  # Change to your preferred availability zone
-
-  tags = {
-    Name = "main-subnet"
-  }
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Change to your preferred AMI
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
 
   tags = {
-    Name = "web-instance"
+    Name = "example-instance"
   }
 }
